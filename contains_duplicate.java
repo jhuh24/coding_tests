eed677a312ec0d444a1c10c8febcd25e3d621c87
Jinsung Huh <jinsunghuh24@berkeley.edu>
2022-08-13 17:44:25 -0700

    contains duplicate

--- contains_duplicate.java
+++ contains_duplicate.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashMap<Integer, Integer> tracker = new HashMap<Integer, Integer>();
-            for (int i : nums) {
-                if (tracker.containsKey(i)) {
+            for (int n : nums) {
+                if (tracker.containsKey(n)) {
                     return true;
                 }
                 else {
-                    tracker.put(i, 1);
+                    tracker.put(n, 1);
                 }
             }
         return false;
