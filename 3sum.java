6aebb11fa89558a10695a402220b98a1e43b5c0e
Jinsung Huh <jinsunghuh24@berkeley.edu>
2022-08-20 23:25:52 -0700

    3sum

--- 3sum.java
+++ 3sum.java
@@ -2,30 +2,26 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         HashSet<Integer> seen = new HashSet<Integer> ();
-        ArrayList<Integer> num = new ArrayList<Integer>();
         List<List<Integer>> answer = new ArrayList();
-        for (int i : nums) {
-            num.add(i);
-        }
+        Set<List<Integer>> sets = new HashSet<>();
         int index1 = 0;
-        while (index1 < num.size()) {
+        while (index1 < nums.length) {
             int index2 = index1 + 1;
-            while (index2 < num.size()) {
-                int opposite = -1 * (num.get(index1) + num.get(index2));
+            while (index2 < nums.length) {
+                int opposite = -1 * (nums[index1] + nums[index2]);
                 if (seen.contains(opposite)) {
-                    int possible_index = num.indexOf(opposite);
-                    ArrayList<Integer> temp = new ArrayList<Integer>();
-                    temp.add(num.get(possible_index));
-                    temp.add(num.get(index1));
-                    temp.add(num.get(index2));
-                    if (!answer.contains(temp)) {
-                        answer.add(temp);
+                    List<Integer> temp = Arrays.asList(opposite, nums[index1], nums[index2]);
+                    if (!sets.contains(temp)) {
+                        sets.add(temp);
                     }
                 }
                 index2 += 1;
             }
             seen.add(nums[index1]);
             index1 +=1;
+        }
+        for(List<Integer> l : sets){
+            answer.add(l);
         }
         return answer;
     }
